caf4805c536385f8820987b0105f3ca28153c0b5
awenafunagain <awen.bourdon@gmail.com>
2024-06-25 15:38:19 +0200

    commit

--- frontvdm/src/components/ProductList.jsx
+++ frontvdm/src/components/ProductList.jsx
@@ -1,13 +1,23 @@
+import styled from 'styled-components';
 import products from './products';
 import Product from './Product';
+
+
+const List = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  gap: 20px;
+`;
 
 const ProductList = () => {
   return (
     <div>
-      <h1>Liste des Produits</h1>
-      {products.map((product) => (
-        <Product key={product.id} product={product} />
-      ))}
+      <h1>La came</h1>
+      <List>
+        {products.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </List>
     </div>
   );
 };
